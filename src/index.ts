ceead8ffb7dd53b6aaf8a7972968f7be1f62a9b7
Paul <pauljavaloyesdev@gmail.com>
2022-05-09 21:49:56 +0200

    Add enviorement variables

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import 'dotenv/config'
 
 const app = express()
+const PORT = process.env.PORT || 3000
+
 app.use(express.json())
-const PORT = 3000
 
 app.get('/ping', (_, res) => {
     console.log("Ping!")
