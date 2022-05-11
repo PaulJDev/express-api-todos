f4b891561c72b04d93197faefe329b5956211e02
Paul <pauljavaloyesdev@gmail.com>
2022-05-11 21:02:42 +0200

    Add getTodoById route

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,13 +1,21 @@
 import express from 'express'
+import { getTodos, getTodoById } from '../services/todo.service'
 
 const router = express.Router()
 
 router.get('/', (_req, res) => {
-    res.send('Fetching all todos')
+    getTodos().then(todos => res.send(todos))
 })
 
 router.post('/', (_req, res) => {
     res.send('Creating a new todo')
+})
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    const todo = await getTodoById(+id)
+
+    todo ? res.send(todo) : res.status(404).send('Todo not found')
 })
 
 export default router
